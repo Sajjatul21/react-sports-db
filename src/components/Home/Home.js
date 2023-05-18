f121e746a58848126f2a6139977965c2315691ce
Sajjatul21 <sajjatulislam565@gmail.com>
2023-05-18 17:51:57 +0100

    fetch data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 const Home = () => {
+    const [players, setPlayers] = useState([]);
     const [search, setSearch] = useState('')
-    console.log(search);
+    // console.log(search);
+    useEffect(() => {
+        fetch(`https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=Danny%20Welbeck`)
+            .then(res => res.json())
+            .then(data => setPlayers(data?.player))
 
-    const handleChange = (event) => {
-        console.log(event.target.value);
+    }, []);
+    console.log(players)
 
-    }
 
     return (
         <div className='home-container'>
             <div className="left-side">
-                {/* <input onChange={event => setSearch(event.target.value)} type="text" className='search-input' /> */}
-                <input onChange={(event) => handleChange(event)} type="text" className='search-input' />
+                {<input onChange={event => setSearch(event.target.value)} type="text" className='search-input' />}
                 <button className='btn-search'>Search</button>
             </div>
 
