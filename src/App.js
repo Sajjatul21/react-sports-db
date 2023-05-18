1a79a7bae9e91d994f9882fbaececd8f5e7d172f
Sajjatul21 <sajjatulislam565@gmail.com>
2023-05-18 17:03:34 +0100

    create nav and home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-
 import './App.css';
-import { useEffect, useState } from 'react';
+import Nav from './components/Nav/Nav';
 
 function App() {
-  // const [players, setPlayers] = useState([]);
-  const [controle, setControle] = useState(false);
-  useEffect(() => {
-    console.log('vai ami achiiiii')
-  }, [controle]);
-  const handleEffect = () => {
-    setControle(!controle)
-  }
-  console.log(controle)
+
   return (
     <div className="App">
-      <button onClick={handleEffect}>click</button>
+      <Nav></Nav>
     </div>
   );
 }
