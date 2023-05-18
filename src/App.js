bd7c9c1d2515450323c6a2a5ef5961ea2c91726b
Sajjatul21 <sajjatulislam565@gmail.com>
2023-05-18 16:53:27 +0100

    start spostr db project and learn clearly useEffect and defendency

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // const [players, setPlayers] = useState([]);
+  const [controle, setControle] = useState(false);
+  useEffect(() => {
+    console.log('vai ami achiiiii')
+  }, [controle]);
+  const handleEffect = () => {
+    setControle(!controle)
+  }
+  console.log(controle)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleEffect}>click</button>
     </div>
   );
 }
