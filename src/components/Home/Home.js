48c4ae23e8fd1f239e69f5e30375d270f886fb06
Sajjatul21 <sajjatulislam565@gmail.com>
2023-05-18 17:13:38 +0100

    set search state and onChange

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
 const Home = () => {
+    const [search, setSearch] = useState('')
+    console.log(search);
     return (
-       <div>
-        
-       </div>
+        <div className='home-container'>
+            <div className="left-side">
+                <input onChange={event => setSearch(event.target.value)} type="text" className='search-input' />
+                <button className='btn-search'>Search</button>
+            </div>
+
+            <div className="right-side">
+                <div className="cart">
+                    <p>This is player Cart</p>
+                </div>
+
+            </div>
+
+        </div>
     );
 };
 export default Home;
